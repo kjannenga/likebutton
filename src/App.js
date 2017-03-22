93845049339322b215638125492d1dbbda545fda
KrystineJannenga <kjannenga1@gmail.com>
2017-03-22 16:32:27 -0700

    make bounce

--- src/App.js
+++ src/App.js
@@ -5,18 +5,31 @@ import './index.css'
 export default React.createClass({
   getInitialState: function(){
     return{
-      qty: 0
+      qty: 0,
+      pi: 'likebtn'
     }
   },
   liker: function(){
     this.setState({
-      qty: this.state.qty + 1
+      qty: this.state.qty + 1,
+      pi: 'btnclick'
+
     })
+
+    setTimeout (function(){
+    this.setState({
+      pi: 'likebtn'
+    })
+
+  }.bind(this), 100)
+    
   },
+  
+
   render: function (){
     return (
       <div>
-        <button id="like" onClick={this.liker} className="likebtn">{this.state.qty} {this.state.qty === 1 ? ' like' : ' likes' }</button>
+        <button id="like" onClick={this.liker} className={this.state.pi}>{this.state.qty} {this.state.qty === 1 ? ' like' : ' likes' }</button>
       </div>
     )
   }
